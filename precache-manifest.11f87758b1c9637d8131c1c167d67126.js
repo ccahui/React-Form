dae56a44a65a2cf0cb7385fb33d6300b7bfadb33
ccahui <kccahui@unsa.edu.pe>
2020-02-24 20:49:08 -0500

    Updates

---
+++ precache-manifest.11f87758b1c9637d8131c1c167d67126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e364323d6bb7dee598ad565e9a324416",
+    "url": "/React-Form/index.html"
+  },
+  {
+    "revision": "1a6d2db32066c8db9cb9",
+    "url": "/React-Form/static/css/main.0053b123.chunk.css"
+  },
+  {
+    "revision": "53e6c45beae4e4cd9615",
+    "url": "/React-Form/static/js/2.73bb5b60.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Form/static/js/2.73bb5b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6d2db32066c8db9cb9",
+    "url": "/React-Form/static/js/main.f1cca10e.chunk.js"
+  },
+  {
+    "revision": "8b47eb0c64eada58c68d",
+    "url": "/React-Form/static/js/runtime-main.b90d3bd6.js"
+  }
+]);
